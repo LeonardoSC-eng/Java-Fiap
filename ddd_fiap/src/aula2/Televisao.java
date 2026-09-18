380c02937f1434d226b2f9dd3725530f4fd7e253
LeonardoSC-eng <leonardoconceicao22@outlook.com>
2026-09-17 23:41:38 -0300

    Aula de Construct

--- ddd_fiap/src/aula2/Televisao.java
+++ ddd_fiap/src/aula2/Televisao.java
@@ -17,17 +17,9 @@ public class Televisao {
     public int getCanal(){
         return canal;
     }
+
     public void setCanal(int canal){
-        if(ligado){
-            if(canal >= 0 && canal <=500) {
-                this.canal = canal;
-                System.out.println("Canal alterado para: "+ this.canal);
-            }else{
-                System.out.println("[Erro]: Canal invalido!");
-            }
-        }else{
-            System.out.println("[AVISO]: A TV está desligada!");
-        }
+        this.canal = canal;
     }
 
     public int getVolume(){
@@ -42,16 +34,6 @@ public class Televisao {
     }
     public void setLigado(boolean ligado){
         this.ligado = ligado;
-    }
-
-    public void ligar(){
-        //Aqui ele está invertendo, se a tv estava ligada, ela desliga e vise-versa
-        this.ligado = !this.ligado;
-        if(ligado){
-            System.out.println(">>> TV Ligada!");
-        }else{
-            System.out.println(">>> TV Desligada!");
-        }
     }
 
 }
